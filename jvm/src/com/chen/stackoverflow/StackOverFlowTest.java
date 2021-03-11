30dc7b02667902f1688d5d1eb59fa43279f4a9c3
chenkun <chenxk@sonoscape.mail>
2021-03-11 20:54:37 +0800

    添加StackOverFlow测试

---
+++ jvm/src/com/chen/stackoverflow/StackOverFlowTest.java
@@ -0,0 +1,21 @@
+package com.chen.stackoverflow;
+
+/**
+ * 测试栈溢出StackOverFlow，不停调用自身方法，直到请求深度超过允许最大深度，为了尽快sof可以给方法定义很多参数，这样参数占用很多表空间，会早点触发溢出
+ * -Xms20m
+ * -Xmx20m
+ * -Xmn10m
+ * -verbose:gc
+ * -XX:+HeapDumpOnOutOfMemoryError
+ * -Xss10m
+ * -XX:+PrintGCDetails
+ */
+public class StackOverFlowTest {
+    public static void main(String[] args) {
+        StackOverFlowTest.testStackOverFlow(3,3,"");
+    }
+
+    public static void testStackOverFlow(int a,int b,String c){
+        testStackOverFlow(3,2,"");
+    }
+}
